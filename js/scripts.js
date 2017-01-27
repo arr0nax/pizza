006f609a3d1fa2b753288decc64e1dfaadb21132
Clayton Collins <claytonccollins@gmail.com>
2017-01-27 11:54:08 -0800

    complete ordering html and begin styling on first form and body

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,12 @@ Pizza.prototype.cost = function() {
   return cost.toString();
 }
 
+Pizza.prototype.display = function() {
+  return ' '+this.toppings+', '+this.size+', $'+this.cost()
+}
+
 var pizzaBook = []
+var pizzaDeliveryBook = []
 
 
 
@@ -30,12 +35,54 @@ $(function() {
     $('#price-display').text(currentPizza.cost());
     pizzaBook.push(currentPizza);
     $('#pizzaList').text('');
-    pizzaBook.forEach(function(pizza) {
-      $('#pizzaList').append('<li>'+pizza.toppings+', '+pizza.size+', $'+pizza.cost()+'<input type="checkbox" class="'+pizza.toppings+'" value="checked"></li>')
-    });
+    for (var i=0;i<pizzaBook.length;i++) {
+      $('#pizzaList').append('<li><input type="checkbox" id="pizza'+i+'" value="checked">'+pizzaBook[i].display()+'</li>')
+    };
   });
-  $('#startDelivery').click(function() {
-    console.log($('#pizzaList').index($('#pizzaList input[type="radio"]:checked')));
-    $('.address').show();
+
+  $('.pricecheck').click(function() {
+    var currentPizza = new Pizza()
+    $('.toppings input[type="checkbox"]:checked').each(function() {
+      currentPizza.toppings.push($(this).val());
+    });
+    currentPizza.sizeCost = parseInt($('.size input[type="radio"]:checked').val());
+    $('#price-display').text(currentPizza.cost());
+  });
+
+  $('#pizzaList').click(function() {
+    $('#deliveryPizzas').text('')
+    for (var i=0;i<pizzaBook.length;i++) {
+      if($('#pizza'+i).prop('checked')) {
+        $('#deliveryPizzas').append('<li>'+pizzaBook[i].display())
+      };
+    };
+    $('#startDelivery').show();
   })
+
+
+
+  $('#startDelivery').click(function() {
+    for (var i=0;i<pizzaBook.length;i++) {
+      if($('#pizza'+i).prop('checked')) {
+        pizzaDeliveryBook.push(pizzaBook[i]);
+      };
+    };
+    $('.address').show();
+  });
+
+
+  $('.address').submit(function(event) {
+    event.preventDefault();
+    var name = $('#name').val();
+    var address = $('#address').val();
+    var requests = $('#requests').val();
+    $('.pizza, .address, .checkout').hide();
+    $('.completed').show();
+    $('#ordercompleted').text('Congratulations '+name+'! Your pizzas are on their way to '+address+'. We are doing our best to do this for you too: '+requests+'. See you soon!')
+    for (var i=0;i<pizzaDeliveryBook.length;i++) {
+      $('#finalorderlist').append('<li>'+pizzaDeliveryBook[i].display()+'</li>');
+    };
+
+
+  });
 });
